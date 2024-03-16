4b437f93e1911b817eb158fa34abc8e50cf4a7aa
Corentin Leruth <corentin.leruth@gmail.com>
2024-03-16 14:55:59 +0100

    part 1 exo 4

--- src/part1-basics/exercises/4-data.ts
+++ src/part1-basics/exercises/4-data.ts
@@ -1,4 +1,5 @@
-import { Effect, Equal, HashSet, Hash, Data, Brand } from "effect";
+import { Effect, Equal, HashSet, Hash, Data, Brand, pipe } from "effect";
+import * as S from "@effect/schema/Schema";
 import assert from "node:assert";
 
 // Exercise 1
@@ -9,6 +10,27 @@ class Transaction implements Equal.Equal, Hash.Hash {
     public readonly amount: number,
     public readonly time: Date
   ) {}
+  [Equal.symbol](that: unknown): boolean {
+    return (
+      that instanceof Transaction &&
+      that.id === this.id &&
+      that.amount === this.amount &&
+      that.time.getTime() === this.time.getTime()
+    );
+  }
+
+  [Hash.symbol](): number {
+    return pipe(
+      Hash.string(this.id),
+      Hash.combine(Hash.number(this.amount)),
+      Hash.combine(Hash.number(this.time.getTime()))
+    );
+    // return Hash.structure({
+    //   id: this.id,
+    //   amount: this.amount,
+    //   time: this.time,
+    // });
+  }
 }
 
 assert(
@@ -27,14 +49,41 @@ assert(
 // Create a datatype for a string that has been guaranteed to be only ascii
 // Here is a regex for you to use : /^[\x00-\x7F]*$/
 
-type ASCIIString = never;
-
-function takesOnlyAscii(s: ASCIIString) {
-  // ...
+class ASCIIString extends Data.TaggedClass("ASCIIString")<{
+  readonly self: string;
+}> {
+  static of(self: string): ASCIIString {
+    if (/^[\x00-\x7F]*$/.test(self)) {
+      return new ASCIIString({ self });
+    } else {
+      throw new Error("Not an ASCII string");
+    }
+  }
 }
 
-const string1: ASCIIString = "hello";
-const string2: ASCIIString = "hello🌍";
+const string1 = ASCIIString.of("hello");
+// const string2 = ASCIIString.of("hello🌍");
 
-takesOnlyAscii(string1);
-takesOnlyAscii(string2);
+// With a brand
+
+type ASCIIString2 = string & Brand.Brand<ASCIIString2>;
+
+const make_ASCIIString2 = Brand.refined<ASCIIString2>(
+  (s) => /^[\x00-\x7F]*$/.test(s),
+  (s) => Brand.error(`${s} is not ASCII`)
+);
+
+const s1 = make_ASCIIString2("hello");
+// const s2 = make_ASCIIString2("hello🌍");
+
+// With schema
+
+const ASCIIString3Brand = Symbol.for("ASCIIString3"); // Ensure uniqueness
+const ASCIIString3 = S.string.pipe(
+  S.pattern(/^[\x00-\x7F]*$/),
+  S.brand(ASCIIString3Brand)
+);
+
+type ASCIIString3 = S.Schema.To<typeof ASCIIString3>; // string & Brand<"ASCIIString3">
+
+const s22 = ASCIIString3("hello🌍");
