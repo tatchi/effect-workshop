6a81e26f9e3aef331816099f721b0a2921d63062
Corentin Leruth <corentin.leruth@gmail.com>
2024-04-10 18:35:46 +0200

    play with rpc

---
+++ src/try-rpc.ts
@@ -0,0 +1,63 @@
+import { Resolver, Router, Rpc } from "@effect/rpc";
+import * as S from "@effect/schema/Schema";
+import {
+  Console,
+  Context,
+  Effect,
+  Layer,
+  ReadonlyArray,
+  Stream,
+  flow,
+} from "effect";
+
+function mainToHandler() {
+  class Greet extends S.TaggedRequest<Greet>()("Greet", S.never, S.string, {
+    name: S.string,
+  }) {}
+
+  class GetName extends S.TaggedRequest<GetName>()(
+    "GetName",
+    S.never,
+    S.string,
+    {}
+  ) {}
+
+  class Repo extends Context.Tag("Repo")<
+    Repo,
+    { readonly getUser: () => Effect.Effect<string> }
+  >() {}
+
+  const router = Router.make(
+    Rpc.effect(Greet, ({ name }) => Effect.succeed(`Hello ${name}!`)),
+    Rpc.effect(GetName, () =>
+      Effect.gen(function* (_) {
+        const { getUser } = yield* _(Repo);
+        return yield* _(getUser());
+      })
+    )
+  ).pipe(
+    Router.provideService(Repo, { getUser: () => Effect.succeed("Morgane") })
+  );
+
+  const handler = Router.toHandler(router);
+
+  const resolver = Resolver.make(handler)<typeof router>();
+
+  const client = Resolver.toClient(resolver);
+
+  return Effect.gen(function* (_) {
+    const greet = yield* _(
+      // Rpc.call(new Greet({ name: "Corentin" }), resolver)
+      client(new Greet({ name: "Corentin!" }))
+    );
+
+    const getName = yield* _(
+      // Rpc.call(new Greet({ name: "Corentin" }), resolver)
+      client(new GetName())
+    );
+
+    console.log({ greet, getName });
+  });
+}
+
+Effect.runPromise(mainToHandler());
